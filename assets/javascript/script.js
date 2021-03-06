3a6c8ad9951a71ec63338f346778f2cf225888c0
DominicSpears <dominicspears24@gmail.com>
2021-03-06 19:18:58 +0000

    begin javascript color change experiments

---
+++ assets/javascript/script.js
@@ -0,0 +1,31 @@
+document.getElementById('start-btn').onclick = function changeContent() {
+
+   document.getElementById('demo').textContent = "Help me";
+   document.getElementById('demo').style = "Color: red";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const cards = document.querySelectorAll(".grid-item");
+
+
+(function shuffle() {
+    cards.forEach(card=> {
+        let randomPos = math.floor(math.random() * 25);
+        card.style.order = randomPos;
+    });
+})();*/
