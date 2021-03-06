adde6fd0ecf83390f13cdd4e026c68ae0795f745
DominicSpears <dominicspears24@gmail.com>
2021-03-06 21:57:25 +0000

    added change background color function to first grid item

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,18 @@
-document.body.style.background = "#f3f3f3"
+//function myFunction() {
+//  document.body.style.backgroundColor = "red";
+//}
 
+function myFunction() {
+  var list = document.getElementsByClassName("grid-container")[0];
+  list.getElementsByClassName("grid-item")[0].style.backgroundColor = "lightblue";
+}
 
+/*var x = document.getElementById("grid-container");
+var y = x.getElementsByClassName("grid-item");
+var i;
+for (i = 0; i < y.length; i++) {
+  y[i].style.backgroundColor = "red";
+}*/
 
 
 
